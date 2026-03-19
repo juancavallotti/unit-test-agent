1c1776edc59efd7488724e8f70b9b47c75a2251c
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2026-03-18 20:05:05 -0700

    Added initial implementation of the agent state

---
+++ src/agent/state.ts
@@ -0,0 +1,10 @@
+import { MessagesValue, StateSchema } from "@langchain/langgraph";
+import * as z from "zod";
+
+/**
+ * The state of the agent.
+ */
+export const State = new StateSchema({
+    messages: MessagesValue,
+    currentCoverage: z.int()
+})
